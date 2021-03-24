cc4567d32243bac044021965242cc1d96d0e7fec
Mollie Munoz <momuno@microsoft.com>
2021-03-24 12:55:50 -0700

    Twin Method sample clean up.

--- iothub_client/samples/iothub_client_device_twin_and_methods_sample/iothub_client_device_twin_and_methods_sample.c
+++ iothub_client/samples/iothub_client_device_twin_and_methods_sample/iothub_client_device_twin_and_methods_sample.c
@@ -1,43 +1,52 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// This sample shows how to translate the Device Twin json received from Azure IoT Hub into meaningful data for your application.
-// It uses the parson library, a very lightweight json parser.
+// This sample shows how to translate the Device Twin document received from Azure IoT Hub into
+// meaningful data for your application. It also shows how to work with Direct Methods and their
+// encoded payloads.
+//
+// There are two encoding options: CBOR or JSON. For CBOR, this sample uses the tinycbor library,
+// and for JSON, it uses the parson library. Both tinycbor and parson are included in the C SDK as
+// submodules, however you may choose your own preferred library to encode/decode the Device Twin
+// document and Direct Methods payloads.
 
-// There is an analogous sample using the serializer - which is a library provided by this SDK to help parse json - in devicetwin_simplesample.
-// Most applications should use this sample, not the serializer.
+// There are analogous samples using the serializer component, which is an SDK library provided to
+// help parse JSON. The serializer is dependent on the parson library, and there is no CBOR support.
+// These samples are devicetwin_simplesample and devicemethod_simplesample. Most applications will
+// not need use of the serializer.
 
-// WARNING: Check the return of all API calls when developing your solution. Return checks ommited for sample simplification.
+// WARNING: Check the return of all API calls when developing your solution. Return checks are
+//          ommited from this sample for simplification.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
 #include "azure_macro_utils/macro_utils.h"
-#include "azure_c_shared_utility/threadapi.h"
 #include "azure_c_shared_utility/platform.h"
-#include "iothub_device_client.h"
-#include "iothub_client_options.h"
+#include "azure_c_shared_utility/threadapi.h"
+
 #include "iothub.h"
+#include "iothub_client_options.h"
+#include "iothub_device_client.h"
 #include "iothub_message.h"
 
 //
-// The sample defaults to encoding the twin document in JSON.  Uncomment to instead use CBOR.
+// Format -- Uncomment the format you wish to use.
 //
-// NOTE:    This sample employees Intel's tinycbor CBOR parsing library. You may choose
-//          whichever CBOR parsing library best suites your application and device.
-//
-#define CONTENT_TYPE_TWIN_CBOR 1
-#ifdef CONTENT_TYPE_TWIN_CBOR
-    #include "cbor.h"  // Intel's tinycbor CBOR parsing library
+#define CONTENT_TYPE_CBOR
+//#define CONTENT_TYPE_JSON
+
+#ifdef CONTENT_TYPE_CBOR
+    #include "cbor.h"
     #define CBOR_BUFFER_SIZE 512
     static uint8_t cbor_buf[CBOR_BUFFER_SIZE];
-#else
+#elif  CONTENT_TYPE_JSON
     #include "parson.h"
-#endif
+#endif // CONTENT TYPE
 
 //
-// Uncomment the protocol you wish to use.
+// Transport Layer Protocal -- Uncomment the protocol you wish to use.
 //
 #define SAMPLE_MQTT
 //#define SAMPLE_MQTT_OVER_WEBSOCKETS
@@ -47,35 +56,30 @@
 
 #ifdef SAMPLE_MQTT
     #include "iothubtransportmqtt.h"
-#endif // SAMPLE_MQTT
-#ifdef SAMPLE_MQTT_OVER_WEBSOCKETS
+#elif SAMPLE_MQTT_OVER_WEBSOCKETS
     #include "iothubtransportmqtt_websockets.h"
-#endif // SAMPLE_MQTT_OVER_WEBSOCKETS
-#ifdef SAMPLE_AMQP
+#elif SAMPLE_AMQP
     #include "iothubtransportamqp.h"
-#endif // SAMPLE_AMQP
-#ifdef SAMPLE_AMQP_OVER_WEBSOCKETS
+#elif SAMPLE_AMQP_OVER_WEBSOCKETS
     #include "iothubtransportamqp_websockets.h"
-#endif // SAMPLE_AMQP_OVER_WEBSOCKETS
-#ifdef SAMPLE_HTTP
+#elif SAMPLE_HTTP
     #include "iothubtransporthttp.h"
-#endif // SAMPLE_HTTP
+#endif // SAMPLE PROTOCOL
 
+//
+// Trusted Cert -- Turn on via build flag
+//
 #ifdef SET_TRUSTED_CERT_IN_SAMPLES
 #include "certs.h"
 #endif // SET_TRUSTED_CERT_IN_SAMPLES
 
-#define DOWORK_LOOP_NUM     3
-
-
-
 //
-// Paste in the your iothub device connection string
+// Connection String - Paste in the your iothub device connection string.
 //
 static const char* connectionString = "[device connection string]";
 
 //
-// Car object
+// Car Object
 //
 typedef struct MAKER_TAG
 {
@@ -92,24 +96,24 @@ typedef struct GEO_TAG
 
 typedef struct CAR_STATE_TAG
 {
-    int64_t softwareVersion;        // reported property
-    uint8_t reportedMaxSpeed;       // reported property
-    unsigned char* vanityPlate;              // reported property
+    int64_t softwareVersion;          // reported property
+    uint8_t reportedMaxSpeed;         // reported property
+    unsigned char* vanityPlate;       // reported property
 } CarState;
 
 typedef struct CAR_SETTINGS_TAG
 {
-    uint8_t desired_maxSpeed;       // desired property
-    Geo location;                   // desired property
+    uint8_t desired_maxSpeed;         // desired property
+    Geo location;                     // desired property
 } CarSettings;
 
 typedef struct CAR_TAG
 {
-    unsigned char* lastOilChangeDate;        // reported property
-    unsigned char* changeOilReminder;        // desired property
-    Maker maker;                    // reported property
-    CarState state;                 // reported property
-    CarSettings settings;           // desired property
+    unsigned char* lastOilChangeDate; // reported property
+    unsigned char* changeOilReminder; // desired property
+    Maker maker;                      // reported property
+    CarState state;                   // reported property
+    CarSettings settings;             // desired property
 } Car;
 
 static void initializeCar(Car* car, unsigned char* lastOilChangeDate, unsigned char* make, unsigned char* style, int64_t year, uint8_t maxSpeed, int64_t swVersion, unsigned char* plate)
@@ -127,7 +131,7 @@ static void initializeCar(Car* car, unsigned char* lastOilChangeDate, unsigned c
 //
 //  Serialize Car object to CBOR/JSON blob. To be sent as a twin document with reported properties.
 //
-#ifdef CONTENT_TYPE_TWIN_CBOR
+#ifdef CONTENT_TYPE_CBOR
 static unsigned char* serializeToCBOR(Car* car)
 {
     CborEncoder cbor_encoder_root;
@@ -166,7 +170,7 @@ static unsigned char* serializeToCBOR(Car* car)
 
     return cbor_buf;
 }
-#else
+#elif CONTENT_TYPE_JSON
 static unsigned char* serializeToJSON(Car* car)
 {
     unsigned char* result;
@@ -189,14 +193,18 @@ static unsigned char* serializeToJSON(Car* car)
 
     return result;
 }
+#endif // CONTENT TYPE
 
-#endif
 
-
-//  Converts the desired properties of the Device Twin JSON blob received from IoT Hub into a Car object.
-#ifdef CONTENT_TYPE_TWIN_CBOR
-#else
-static Car* parseFromJson(const char* json, DEVICE_TWIN_UPDATE_STATE update_state)
+//
+// Convert the desired properties of the Device Twin CBOR/JSON blob from IoT Hub into a Car Object.
+//
+#ifdef CONTENT_TYPE_CBOR
+static Car* parseFromCBOR(const char* cbor, DEVICE_TWIN_UPDATE_STATE update_state)
+{
+}
+#elif CONTENT_TYPE_JSON
+static Car* parseFromJSON(const char* json, DEVICE_TWIN_UPDATE_STATE update_state)
 {
     Car* car = malloc(sizeof(Car));
     JSON_Value* root_value = NULL;
@@ -268,49 +276,29 @@ static Car* parseFromJson(const char* json, DEVICE_TWIN_UPDATE_STATE update_stat
 
     return car;
 }
-#endif
+#endif // CONTENT TYPE
 
-static int deviceMethodCallback(const char* method_name, const unsigned char* payload, size_t size, unsigned char** response, size_t* response_size, void* userContextCallback)
-{
-    (void)userContextCallback;
-    (void)payload;
-    (void)size;
+//
+// Callbacks
+//
 
-    int result;
-
-    if (strcmp("getCarVIN", method_name) == 0)
-    {
-        const char deviceMethodResponse[] = "{ \"Response\": \"1HGCM82633A004352\" }";
-        *response_size = sizeof(deviceMethodResponse)-1;
-        *response = malloc(*response_size);
-        (void)memcpy(*response, deviceMethodResponse, *response_size);
-        result = 200;
-    }
-    else
-    {
-        // All other entries are ignored.
-        const char deviceMethodResponse[] = "{ }";
-        *response_size = sizeof(deviceMethodResponse)-1;
-        *response = malloc(*response_size);
-        (void)memcpy(*response, deviceMethodResponse, *response_size);
-        result = -1;
-    }
-
-    return result;
-}
-
-static void getCompleteDeviceTwinOnDemandCallback(DEVICE_TWIN_UPDATE_STATE update_state, const unsigned char* payLoad, size_t size, void* userContextCallback)
+// Callback for async GET request to IoT Hub for entire Device Twin document.
+static void getTwinAsyncCallback(DEVICE_TWIN_UPDATE_STATE update_state, const unsigned char* payLoad, size_t size, void* userContextCallback)
 {
     (void)update_state;
     (void)userContextCallback;
     printf("GetTwinAsync result:\r\n%.*s\r\n", (int)size, payLoad);
 }
 
-static void deviceTwinCallback(DEVICE_TWIN_UPDATE_STATE update_state, const unsigned char* payLoad, size_t size, void* userContextCallback)
+// Callback for when IoT Hub updates the desired properties of the Device Twin document.
+static void deviceDesiredPropertiesTwinCallback(DEVICE_TWIN_UPDATE_STATE update_state, const unsigned char* payLoad, size_t size, void* userContextCallback)
 {
     (void)update_state;
     (void)size;
-/*
+
+#ifdef CONTENT_TYPE_CBOR
+
+#elif CONTENT_TYPE_JSON
     Car* oldCar = (Car*)userContextCallback;
     Car* newCar = parseFromJson((const char*)payLoad, update_state);
 
@@ -367,37 +355,66 @@ static void deviceTwinCallback(DEVICE_TWIN_UPDATE_STATE update_state, const unsi
 
         free(newCar);
     }
-    */
+#endif // CONTENT TYPE
 }
 
-static void reportedStateCallback(int status_code, void* userContextCallback)
+// Callback for when device sends reported properties to IoT Hub, and IoT Hub updates the Device
+// Twin document.
+static void deviceReportedPropertiesTwinCallback(int status_code, void* userContextCallback)
 {
     (void)userContextCallback;
     printf("Device Twin reported properties update completed with result: %d\r\n", status_code);
 }
 
+// Callback for when IoT Hub sends a Direct Method to the device.
+static int deviceMethodCallback(const char* method_name, const unsigned char* payload, size_t size, unsigned char** response, size_t* response_size, void* userContextCallback)
+{
+    (void)userContextCallback;
+    (void)payload;
+    (void)size;
+
+    int result;
+
+    if (strcmp("getCarVIN", method_name) == 0)
+    {
+        const char deviceMethodResponse[] = "{ \"Response\": \"1HGCM82633A004352\" }";
+        *response_size = sizeof(deviceMethodResponse)-1;
+        *response = malloc(*response_size);
+        (void)memcpy(*response, deviceMethodResponse, *response_size);
+        result = 200;
+    }
+    else
+    {
+        // All other entries are ignored.
+        const char deviceMethodResponse[] = "{ }";
+        *response_size = sizeof(deviceMethodResponse)-1;
+        *response = malloc(*response_size);
+        (void)memcpy(*response, deviceMethodResponse, *response_size);
+        result = -1;
+    }
+
+    return result;
+}
 
 static void iothub_client_device_twin_and_methods_sample_run(void)
 {
     IOTHUB_CLIENT_TRANSPORT_PROVIDER protocol;
     IOTHUB_DEVICE_CLIENT_HANDLE iotHubClientHandle;
 
-    // Select the Protocol to use with the connection
+    //
+    // Select the Transport Layer Protocal
+    //
 #ifdef SAMPLE_MQTT
     protocol = MQTT_Protocol;
-#endif // SAMPLE_MQTT
-#ifdef SAMPLE_MQTT_OVER_WEBSOCKETS
+#elif SAMPLE_MQTT_OVER_WEBSOCKETS
     protocol = MQTT_WebSocket_Protocol;
-#endif // SAMPLE_MQTT_OVER_WEBSOCKETS
-#ifdef SAMPLE_AMQP
+#elif SAMPLE_AMQP
     protocol = AMQP_Protocol;
-#endif // SAMPLE_AMQP
-#ifdef SAMPLE_AMQP_OVER_WEBSOCKETS
+#elif SAMPLE_AMQP_OVER_WEBSOCKETS
     protocol = AMQP_Protocol_over_WebSocketsTls;
-#endif // SAMPLE_AMQP_OVER_WEBSOCKETS
-#ifdef SAMPLE_HTTP
+#elif SAMPLE_HTTP
     protocol = HTTP_Protocol;
-#endif // SAMPLE_HTTP
+#endif // SAMPLE PROTOCOL
 
     if (IoTHub_Init() != 0)
     {
@@ -414,49 +431,54 @@ static void iothub_client_device_twin_and_methods_sample_run(void)
             //
             // Set Options
             //
-
-            // Debugging
-            bool traceOn = true;
+            bool traceOn = true; // Debugging
             (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_LOG_TRACE, &traceOn);
 
-#if defined SAMPLE_MQTT || defined SAMPLE_MQTT_OVER_WEBSOCKETS
-    #ifdef CONTENT_TYPE_TWIN_CBOR
-            // Format direct method payloads and twin document using CBOR.
+#ifdef SAMPLE_MQTT || defined SAMPLE_MQTT_OVER_WEBSOCKETS
+            // Set the auto URL Encoder (recommended for MQTT). Please use this option unless you
+            // are URL Encoding inputs yourself. ONLY valid for use with MQTT.
+            bool urlEncodeOn = true;
+            (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_AUTO_URL_ENCODE_DECODE, &urlEncodeOn);
+#ifdef CONTENT_TYPE_CBOR
+            // Format Device Twin document and Direct Method payload using CBOR.
             // ONLY valid for use with MQTT. Must occur prior to CONNECT.
             OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE ct = OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE_CBOR;
             (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_METHOD_TWIN_CONTENT_TYPE, &ct);
-    #else
-            // Not necessary to set to JSON, but this is still expected to work.
-            //OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE ct = OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE_JSON;
-            //(void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_METHOD_TWIN_CONTENT_TYPE, &ct);
-    #endif
-            // Set the auto URL Encoder (recommended for MQTT). Please use this option unless you are URL Encoding inputs yourself.
-            // ONLY valid for use with MQTT.
-            bool urlEncodeOn = true;
-            (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_AUTO_URL_ENCODE_DECODE, &urlEncodeOn);
-#endif
+#elif CONTENT_TYPE_JSON
+            // This option not required to use JSON format due to backwards compatibility.
+            // If option is used, it is ONLY valid for use with MQTT. Must occur priot to CONNECT.
+            OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE ct = OPTION_METHOD_TWIN_CONTENT_TYPE_VALUE_JSON;
+            (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, OPTION_METHOD_TWIN_CONTENT_TYPE, &ct);
+#endif // CONTENT TYPE
+#endif // SAMPLE_MQTT || SAMPLE_MQTT_OVER_WEBSOCKETS
+
 #ifdef SET_TRUSTED_CERT_IN_SAMPLES
-            // For mbed add the certificate information
-            if (IoTHubDeviceClient_SetOption(iotHubClientHandle, "TrustedCerts", certificates) != IOTHUB_CLIENT_OK)
-            {
-                (void)printf("failure to set option \"TrustedCerts\"\r\n");
-            }
+            (void)IoTHubDeviceClient_SetOption(iotHubClientHandle, "TrustedCerts", certificates)
 #endif // SET_TRUSTED_CERT_IN_SAMPLES
 
+            //
+            // Create Car Object
+            //
             Car car;
             initializeCar(&car, "2016", "Fabrikam", "sedan", 2014, 100, 1, "1I1");
 
-#ifdef CONTENT_TYPE_TWIN_CBOR
+#ifdef CONTENT_TYPE_CBOR
             unsigned char* reportedProperties = serializeToCBOR(&car);
-#else
+#elif CONTENT_TYPE_JSON
             unsigned char* reportedProperties = serializeToJSON(&car);
-#endif
+#endif // CONTENT TYPE
 
-            //(void)IoTHubDeviceClient_GetTwinAsync(iotHubClientHandle, getCompleteDeviceTwinOnDemandCallback, NULL);
-            (void)IoTHubDeviceClient_SendReportedState(iotHubClientHandle, reportedProperties, strlen(reportedProperties), reportedStateCallback, NULL);
-            //(void)IoTHubDeviceClient_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
-            //(void)IoTHubDeviceClient_SetDeviceTwinCallback(iotHubClientHandle, deviceTwinCallback, &car);
+            //
+            // Send and receive messages from IoT Hub
+            //
+            (void)IoTHubDeviceClient_GetTwinAsync(iotHubClientHandle, getTwinAsyncCallback, NULL);
+            (void)IoTHubDeviceClient_SendReportedState(iotHubClientHandle, reportedProperties, strlen(reportedProperties), deviceReportedPropertiesTwinCallback, NULL);
+            (void)IoTHubDeviceClient_SetDeviceTwinCallback(iotHubClientHandle, deviceDesiredPropertiesTwinCallback, &car);
+            (void)IoTHubDeviceClient_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
 
+            //
+            // Exit
+            //
             (void)printf("Press any key to exit sample.\r\n");
             (void)getchar();
 
